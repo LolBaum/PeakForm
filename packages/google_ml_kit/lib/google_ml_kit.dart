df10c5600375b98316515d806b08f2dec9e74aa1
wurzel <og.og.og.plo@gmail.com>
2025-06-10 10:44:45 +0200

    fix: add missing packages

---
+++ packages/google_ml_kit/lib/google_ml_kit.dart
@@ -0,0 +1,6 @@
+export 'package:google_mlkit_commons/google_mlkit_commons.dart';
+export 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+
+export 'src/google_ml_kit.dart';
+export 'src/natural_language.dart';
+export 'src/vision.dart';
